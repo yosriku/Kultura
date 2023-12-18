901fbf329dd15260c7d7f30780a5d7206231ffb5
Gideon061 <103176588+Gideon061@users.noreply.github.com>
2023-12-19 06:32:13 +0700

    JSON Revision

--- CC/app/utils.py
+++ CC/app/utils.py
@@ -8,53 +8,53 @@ products = {
     "message":"success",
     "topeng":[
         {
-        "id":1,
-        "Kisaran Harga":"Rp. 100.000 - Rp. 130.000",
-        "informasi":"Topeng spesialis lucu di antara topeng Bali lainnya seperti topeng tua dan topeng keras",
-        "name":"Topeng Bujuh",
-        "image-url":'https://storage.googleapis.com/kultura-image/topeng-bujuh.png'
+            "Kisaran Harga":"Rp. 100.000 - Rp. 130.000",
+            "id":1,
+            "image-url":"https://storage.googleapis.com/kultura-image/topeng-bujuh.png",
+            "informasi":"Topeng spesialis lucu di antara topeng Bali lainnya seperti topeng tua dan topeng keras",
+            "name":"Topeng Bujuh"
         },
         {
-        "id":2,
-        "Kisaran Harga":"Rp. 135.000 - Rp. 150.000",
-        "informasi":"Topeng Dalem Arsa Wijaya berwajah manis pada Topeng Wali, melambangkan pemimpin yang membawa harapan (arsa) dan kemenangan (wijaya) bagi rakyatnya. Topeng Arsa Wijaya Secara visual, rupa topeng ini berwajah tampan, bermata sipit, memakai urna, dominan berwarna putih.",
-        "name":"Topeng Dalem Arsa Wijaya",
-        "image-url":'https://storage.googleapis.com/kultura-image/topeng-dalem.jpeg'
+            "Kisaran Harga":"Rp. 135.000 - Rp. 150.000",
+            "id":2,
+            "image-url":"https://storage.googleapis.com/kultura-image/topeng-dalem.jpeg",
+            "informasi":"Topeng Dalem Arsa Wijaya berwajah manis pada Topeng Wali, melambangkan pemimpin yang membawa harapan (arsa) dan kemenangan (wijaya) bagi rakyatnya. Topeng Arsa Wijaya Secara visual, rupa topeng ini berwajah tampan, bermata sipit, memakai urna, dominan berwarna putih.",
+            "name":"Topeng Dalem Arsa Wijaya"
         },
         {
-        "id":3,
-        "Kisaran Harga":"Rp. 150.000 - Rp. 180.000",
-        "informasi":"Topeng keras biasa lebih dikenal sebagai sosok petarung.",
-        "name":"Topeng Keras",
-        "image-url":'https://storage.googleapis.com/kultura-image/topeng-keras.jpg'
+            "Kisaran Harga":"Rp. 150.000 - Rp. 180.000",
+            "id":3,
+            "image-url":"https://storage.googleapis.com/kultura-image/topeng-keras.jpg",
+            "informasi":"Topeng keras biasa lebih dikenal sebagai sosok petarung.",
+            "name":"Topeng Keras"
         },
         {
-        "id":4,
-        "Kisaran Harga":"Rp.100.000 - Rp. 120.000",
-        "informasi":"tokoh penasar (penutur cerita)",
-        "name":"Topeng Penasar",
-        "image-url":'https://storage.googleapis.com/kultura-image/topeng-penasar.jpg'
+            "Kisaran Harga":"Rp.100.000 - Rp. 120.000",
+            "id":4,
+            "image-url":"https://storage.googleapis.com/kultura-image/topeng-penasar.jpg",
+            "informasi":"tokoh penasar (penutur cerita)",
+            "name":"Topeng Penasar"
         },
         {
-        "id":5,
-        "Kisaran Harga":"Rp.115.000 - Rp.150.000",
-        "informasi":"Topeng berasal dari kata tup yang artinya tutup. Sidakarya berasal dari kata \"sida\" yang artinya mencapai, dan \"karya\" yang artinya tujuan atau pekerjaan. Sidakarya memiliki makna mencapai tujuan atau menyelesaikan pekerjaan",
-        "name":"Topeng Sidakarya",
-        "image-url":['https://storage.googleapis.com/kultura-image/topeng-sidakarya-1.jpeg','https://storage.googleapis.com/kultura-image/topeng-sidakarya-2.jpg']
+            "Kisaran Harga":"Rp.115.000 - Rp.150.000",
+            "id":5,
+            "image-url": "https://storage.googleapis.com/kultura-image/topeng-sidakarya-1.jpeg",
+            "informasi":"Topeng berasal dari kata tup yang artinya tutup. Sidakarya berasal dari kata \"sida\" yang artinya mencapai, dan \"karya\" yang artinya tujuan atau pekerjaan. Sidakarya memiliki makna mencapai tujuan atau menyelesaikan pekerjaan",
+            "name":"Topeng Sidakarya"
         },
         {
-        "id":6,
-        "Kisaran Harga":"Rp.145.000 - Rp.210.000",
-        "informasi":"Topeng tua biasanya digunakan dalam pentas seni dalam ritual peringatan piodalan.",
-        "name":"Topeng Tua",
-        "image-url":['https://storage.googleapis.com/kultura-image/topeng-tua-1.jpg','https://storage.googleapis.com/kultura-image/topeng-tua-2.jpeg']
+            "Kisaran Harga":"Rp.145.000 - Rp.210.000",
+            "id":6,
+            "image-url": "https://storage.googleapis.com/kultura-image/topeng-tua-1.jpg",
+            "informasi":"Topeng tua biasanya digunakan dalam pentas seni dalam ritual peringatan piodalan.",
+            "name":"Topeng Tua"
         },
         {
-        "id":7,
-        "Kisaran Harga":"Rp.250.000",
-        "informasi":"Karkater wijil merupakan karakter punakawan yang perpasangan dengan Penasar. Mereka adalah pendamping atau abdi raja dalam kesenian topeng wali",
-        "name":"Topeng Wijil",
-        "image-url":'https://storage.googleapis.com/kultura-image/topeng-wijil.jpeg'
-    }
+            "Kisaran Harga":"Rp.250.000",
+            "id":7,
+            "image-url":"https://storage.googleapis.com/kultura-image/topeng-wijil.jpeg",
+            "informasi":"Karkater wijil merupakan karakter punakawan yang perpasangan dengan Penasar. Mereka adalah pendamping atau abdi raja dalam kesenian topeng wali",
+            "name":"Topeng Wijil"
+        }
     ]
-    }
+}
